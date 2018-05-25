3e82c78159772845a8242432ce5aaf618ae01c6f
JohnVictor2017 <30505330+JohnVictor2017@users.noreply.github.com>
2018-05-25 13:51:24 -0300

    Correções do pre_processamento
    Funções criadas:
    void detalharInscrito(Inscritos i);
    int contarCategorias(Inscritos inscrito[], Categorias c[],int n);
    
    +Organização do código;

--- GeradorTabelas/main.cpp
+++ GeradorTabelas/main.cpp
@@ -9,6 +9,30 @@
 
 using namespace std;
 
+struct Inscritos
+{
+    int id;
+    string clube;
+    string id_virtual;
+    string atleta_nome;
+    string ranking;
+    string rankingP;
+    string rating;
+    string ratingP;
+    string participaRAT;
+    string status;
+};
+
+struct Categorias
+{
+    string categoria;
+    int quantidade_inscritos;
+};
+
+
+void detalharInscrito(Inscritos i);
+int contarCategorias(Inscritos inscrito[], Categorias c[],int n);
+
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
@@ -18,79 +42,83 @@ int main()
     int n;
     int qt=0;
 
-    struct inscritos
-    {
-        int id;
-        string clube;
-        string id_virtual;
-        string atleta_nome;
-        string ranking;
-        string rankingP;
-        string rating;
-        string ratingP;
-        string participaRAT;
-        string status;
-    };
 
-    struct categorias
-    {
-        string categoria;
-    };
 
 
     //string clubes[MAX_N][MAX_TXT];
     //array<int,100> clubes;
-    inscritos inscrito[100];
-    categorias c[100];
+    Inscritos inscrito[100];
+    Categorias c[100];
 
 
 
-    ifstream ip("\inscritos.csv");
+    ifstream ip("\inscritos2.csv");
     if(ip.is_open())
     {
         while(ip.good())
         {
-
-            inscrito[qt].id=qt;
-            getline(ip,inscrito[qt].clube,';');
-            //ip.getline(clubes[qt],';');
-            getline(ip,inscrito[qt].id_virtual,';');
-            getline(ip,inscrito[qt].atleta_nome,';');
-            getline(ip,inscrito[qt].ranking,';');
-            getline(ip,inscrito[qt].rankingP,';');
-            getline(ip,inscrito[qt].rating,';');
-            getline(ip,inscrito[qt].ratingP,';');
-            getline(ip,inscrito[qt].participaRAT,';');
-            getline(ip,inscrito[qt].status,'\n');
-
-            //clubes.at(i)=clube;
-            //clubes[quant_inscritos]=clube;
-
-
-
             if(qt==0)
             {
                 qt++;
                 continue;
             }
 
+            Inscritos t;
 
-            cout<<"============================="<<endl;
-            cout<<"nº inscrito: "<<inscrito[qt].id<<endl;
-            cout<<"Clube: "<<inscrito[qt].clube<<endl;
-            cout<<"Atleta: "<<inscrito[qt].atleta_nome<<endl;
-            cout<<"Ranking: "<<inscrito[qt].ranking<<endl;
-            cout<<"Rating: "<<inscrito[qt].rating<<endl;
-            cout<<"pontos_RAT: "<<inscrito[qt].ratingP<<endl;
-            cout<<"participa RAT: "<<inscrito[qt].participaRAT<<endl;
-            cout<<"status: "<<inscrito[qt].status<<endl;
-            cout<<"============================="<<endl;
+            t.id=qt;
+            getline(ip,t.clube,';');
+            getline(ip,t.id_virtual,';');
+            getline(ip,t.atleta_nome,';');
+            getline(ip,t.ranking,';');
+            getline(ip,t.rankingP,';');
+            getline(ip,t.rating,';');
+            getline(ip,t.ratingP,';');
+            getline(ip,t.participaRAT,';');
+            getline(ip,t.status,'\n');
+            if(t.ratingP=="" || t.status!="Inscrito")
+            {
+                //cout<<"!!!TECNICO!!!";
+                //não pegar quando for técnico e tecnico não possuem pontos de rating no momento;
+                //não pegar os pre-inscritos;
+            }
+            else
+            {
 
-            qt++;
+                inscrito[qt]=t;
+
+                //detalharInscrito(inscrito[qt]);
+
+                qt++;
+
+            }
+
+            //inscrito[qt].id=qt;
+            //getline(ip,inscrito[qt].clube,';');
+            //ip.getline(clubes[qt],';');
+            //getline(ip,inscrito[qt].id_virtual,';');
+            //getline(ip,inscrito[qt].atleta_nome,';');
+            //getline(ip,inscrito[qt].ranking,';');
+            //getline(ip,inscrito[qt].rankingP,';');
+            //getline(ip,inscrito[qt].rating,';');
+            //getline(ip,inscrito[qt].ratingP,';');
+            //getline(ip,inscrito[qt].participaRAT,';');
+            //getline(ip,inscrito[qt].status,'\n');
+
+            //clubes.at(i)=clube;
+            //clubes[quant_inscritos]=clube;
+
+
+
+
+
+
+
+
+
 
         }
+
         inscrito[qt].id='\0';
-        cout<<qt-1<<" inscrito(s)!";
         ip.close();
 
 
@@ -102,12 +130,73 @@ int main()
         cout<<"ERRO!";
     }
 
+    int cont=contarCategorias(inscrito,c,qt-1);
+
+
+
+    cout<<"---RELATÓRIO DE INSCRITOS---"<<endl;
+
+    int a=0;
+    for(int j=0; j<=cont; j++)
+    {
+        a=0;
+        cout<<"==="<<c[j].categoria<<"==="<<endl;
+        for(int i=1; i<=qt-1; i++)
+        {
+            if(inscrito[i].ranking==c[j].categoria)
+            {
+                a++;
+                cout<<"atleta: "<<inscrito[i].clube<<" | "<<inscrito[i].atleta_nome<<" | pontos:"<<inscrito[i].ratingP<<endl;
+
+            }
+            if(inscrito[i].rating==c[j].categoria && inscrito[i].participaRAT=="SIM")
+            {
+                a++;
+                cout<<"atleta: "<<inscrito[i].clube<<" | "<<inscrito[i].atleta_nome<<" | pontos:"<<inscrito[i].ratingP<<endl;
+
+            }
+
+
+        }
+        c[j].quantidade_inscritos=a;
+        cout<<c[j].quantidade_inscritos<<" atleta(s)";
+        cout<<endl<<endl;
+    }
+
+
+
+
+
+
+    return 0;
+}
+
+void detalharInscrito(Inscritos i)
+{
+
+    cout<<"============================="<<endl;
+    cout<<"nº inscrito: "<<i.id<<endl;
+    cout<<"Clube: "<<i.clube<<endl;
+    cout<<"Atleta: "<<i.atleta_nome<<endl;
+    cout<<"Ranking: "<<i.ranking<<endl;
+    cout<<"Rating: "<<i.rating<<endl;
+    cout<<"pontos_RAT: "<<i.ratingP<<endl;
+    cout<<"participa RAT: "<<i.participaRAT<<endl;
+    cout<<"status: "<<i.status<<endl;
+    cout<<"============================="<<endl;
+
+
+
+}
+
+int contarCategorias(Inscritos inscrito[], Categorias c[],int n)
+{
     int cont=0;
     bool flag_RANK =false,flag_RAT=false;;
     c[cont].categoria=inscrito[1].ranking;
     //cout<<"structs:"<<sizeof(struct inscritos);
 
-    for(int i=1; i<qt-1; i++)
+    for(int i=1; i<n; i++)
     {
         flag_RANK=false;
         for(int j=0; j<=cont; j++)
@@ -129,7 +218,7 @@ int main()
         }
 
     }
-    for(int i=1; i<qt-1; i++)
+    for(int i=1; i<n; i++)
     {
         flag_RAT=false;
         for(int j=0; j<=cont; j++)
@@ -149,49 +238,12 @@ int main()
         }
     }
 
+    //cout<<"categorias:"<<endl;
+    //for(int i=0; i<=cont; i++)
+    //{
+    //    cout<<c[i].categoria<<endl;
+    // }
+    return cont;
 
-
-
-
-
-
-    cout<<"categorias:"<<endl;
-    for(int i=0; i<=cont; i++)
-    {
-        cout<<c[i].categoria<<endl;
-    }
-
-
-    int a=0;
-    for(int j=0; j<=cont; j++)
-    {
-        a=0;
-        cout<<"==="<<c[j].categoria<<"==="<<endl;
-        for(int i=1; i<=qt-1; i++)
-        {
-            if(inscrito[i].ranking==c[j].categoria)
-            {
-                a++;
-                cout<<"atleta: "<<inscrito[i].clube<<" | "<<inscrito[i].atleta_nome<<" | pontos:"<<inscrito[i].ratingP<<endl;
-
-            }
-            if(inscrito[i].rating==c[j].categoria)
-            {
-                a++;
-                cout<<"atleta: "<<inscrito[i].clube<<" | "<<inscrito[i].atleta_nome<<" | pontos:"<<inscrito[i].ratingP<<endl;
-
-            }
-
-
-        }
-        cout<<a<<" atleta(s)";
-        cout<<endl<<endl;
-    }
-
-
-
-
-
-
-    return 0;
 }
+
